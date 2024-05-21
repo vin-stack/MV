58173f54e34db5f49f68e801daa32a6c90b4bfd3
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-21 18:48:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from streamlit_option_menu import  option_menu
 from streamlit_extras.customize_running import center_running
 import time
 
-
 def extract_all_files(zip_ref, temp_dir):
     files = []
     for root, _, filenames in os.walk(temp_dir):
@@ -75,7 +74,16 @@ def chat_with_model(query):
             return f"Error: Received status code {response.status_code}\nResponse: {response.text}"
     except requests.exceptions.RequestException as e:
         return f"Error: {e}"
-
+def main():
+    with st.sidebar:
+        choice = option_menu("MASTER VECTORS", ["Train MV","Chat"], 
+        icons=['upload','chat'], menu_icon="server", default_index=1,orientation="Vertical")
+    if choice == "Train MV":
+        zip_extractor()
+    elif choice == "Chat":
+        example()
+   
+    	
 	
 
 
@@ -143,18 +151,6 @@ def example():
         elif message["role"] == "user":
             
             st.write(f"**👧🏻User:** {message['content']}")
-            
-def main():
-    with st.sidebar:
-        choice = option_menu("MASTER VECTORS", ["Train MV","Chat"], 
-                             
-        icons=['upload','chat'], menu_icon="server", default_index=1,orientation="Vertical")
-        if choice == "Train MV":
-            zip_extractor()
-        elif choice == "Chat":
-            example()
-   
-    	
 
 if __name__ == '__main__':
     main()
