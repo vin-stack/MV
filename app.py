a826a16bc2648237bb1f1c94a0800dddeac93e86
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-21 18:22:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,8 +73,8 @@ def chat_with_model(query):
         return f"Error: {e}"
 def main():
     with st.sidebar:
-    		choice = option_menu("SQLSPACE", ["Sign Up","Sign In"], 
-        	icons=['person','key'], menu_icon="server", default_index=1,orientation="horizontal")
+    		choice = option_menu("SQLSPACE", ["Train MV","Chat MV"], 
+        	icons=['data','chat'], menu_icon="server", default_index=1,orientation="horizontal")
     if choice == "Zip Extractor":
         zip_extractor()
     elif choice == "Chat":
