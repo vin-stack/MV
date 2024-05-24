43ed76a2d839dbb8f9c6a4dd5451eb2d5967d75e
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-24 09:08:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -217,12 +217,11 @@ def zip_extractor():
                             status_code, response_text = result
                             st.write(f"Status: {status_code}, Response: {response_text}")
                             log_entry = {
-                                "filename": filename,
                                 "collection": collection,
                                 "type": doc_type,
                                 "status_code": status_code,  # Assuming success for simplicity
                                 "message": response_text,
-                                "timestamp": timestamp
+                                
                             }
                             add_log(log_entry)
                         # Add logs for each processed file
@@ -231,13 +230,10 @@ def zip_extractor():
                             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                             log_entry = {
                                 "filename": filename,
-                                "collection": collection,
-                                "type": doc_type,
-                                "status_code": status_code,  # Assuming success for simplicity
-                                "message": response_text,
+                                
                                 "timestamp": timestamp
                             }
-                            #add_log(log_entry)
+                            add_log(log_entry)
                 else:
                     st.error("Please enter both collection name and type.")
 
