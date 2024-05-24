e5b5b052aed72aa4f2ef7f178facbbd321a2830e
MR.CHAY <84063946+vin-stack@users.noreply.github.com>
2024-05-24 09:28:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -233,7 +233,8 @@ def zip_extractor():
                                 
                                 "timestamp": timestamp
                             }
-                            add_log(log_entry1+log_entry2)
+                            log_entry=log_entry1+log_entry2
+                            add_log(log_entry)
                 else:
                     st.error("Please enter both collection name and type.")
 
